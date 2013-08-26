d1a59de92eb65b7b5c7d7493e0efbc7737365f54
JulianeAlbuquerque <juliane.albq@gmail.com>
2013-08-26 11:35:37 -0300

    deixando a lista apenas com a primeira letra em maiúsculo

--- spec/country_select_spec.rb
+++ spec/country_select_spec.rb
@@ -23,17 +23,17 @@ module ActionView
 
       let(:selected_us_option) do
         if defined?(Tags::Base)
-          content_tag(:option, 'VENEZUELANA', :selected => :selected, :value => "VENEZUELANA")
+          content_tag(:option, 'Venezuelana', :selected => :selected, :value => "Venezuelana")
         else
-          "<option value=\"VENEZUELANA\" selected=\"selected\">VENEZUELANA</option>"
+          "<option value=\"Venezuelana\" selected=\"selected\">Venezuelana</option>"
         end
       end
 
       let(:selected_iso_us_option) do
         if defined?(Tags::Base)
-          content_tag(:option, 'VENEZUELANA', :selected => :selected, :value => '159')
+          content_tag(:option, 'Venezuelana', :selected => :selected, :value => '159')
         else
-          "<option value=\"159\" selected=\"selected\">VENEZUELANA</option>"
+          "<option value=\"159\" selected=\"selected\">Venezuelana</option>"
         end
       end
 
@@ -60,27 +60,27 @@ module ActionView
           end
 
           it "selects the value of country_name" do
-            walrus.country_name = 'VENEZUELANA'
+            walrus.country_name = 'Venezuelana'
             t = builder.country_select(:country_name)
             t.should include(selected_us_option)
           end
         end
 
         describe "#priority_countries" do
-          let(:tag) { builder.country_select(:country_name, ['VENEZUELANA']) }
+          let(:tag) { builder.country_select(:country_name, ['Venezuelana']) }
 
           it "puts the countries at the top" do
-            tag.should include("#{select_tag}<option value=\"VENEZUELANA")
+            tag.should include("#{select_tag}<option value=\"Venezuelana")
           end
 
           it "inserts a divider" do
-            tag.should include(">VENEZUELANA</option><option value=\"\" disabled=\"disabled\">-------------</option>")
+            tag.should include(">Venezuelana</option><option value=\"\" disabled=\"disabled\">-------------</option>")
           end
 
           it "does not mark two countries as selected" do
-            walrus.country_name = "VENEZUELANA"
+            walrus.country_name = "Venezuelana"
             str = <<-EOS.strip
-              </option>\n<option value="VENEZUELANA" selected="selected">VENEZUELANA</option>
+              </option>\n<option value="Venezuelana" selected="selected">Venezuelana</option>
             EOS
             tag.should_not include(str)
           end
@@ -116,13 +116,13 @@ module ActionView
           end
 
           it "inserts a divider" do
-            tag.should include(">VENEZUELANA</option><option value=\"\" disabled=\"disabled\">-------------</option>")
+            tag.should include(">Venezuelana</option><option value=\"\" disabled=\"disabled\">-------------</option>")
           end
 
           it "does not mark two countries as selected" do
             walrus.country_name = "159"
             str = <<-EOS.strip
-              </option>\n<option value="159" selected="selected">VENEZUELANA</option>
+              </option>\n<option value="159" selected="selected">Venezuelana</option>
             EOS
             tag.should_not include(str)
           end
